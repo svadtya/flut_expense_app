03beb41241f7515d8fbec7d44f650c2b805c1d63
siva <mdsiva.surya@gmail.com>
2023-03-08 10:44:05 +0800

    mapping transaction data into card widgets

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   final List<Transaction> transactions = [
     Transaction(
-        id: 't1', 
-        title: 'New Shoes', 
-        amount: 69.99, 
-        date: DateTime.now()),
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
     Transaction(
         id: 't2',
         title: 'Weekly Groceries',
@@ -46,9 +43,10 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            Card(
-              color: Colors.red,
-              child: Text('LIST OF TX'),
+            Column(
+              children: transactions.map((tx) {
+                return Card(child: Text(tx.title),);
+              }).toList(),
             )
           ],
         ));
