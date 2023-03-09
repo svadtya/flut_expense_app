f373e799921d738b2c4043fb5f96ae052387323c
siva <mdsiva.surya@gmail.com>
2023-03-09 11:37:27 +0800

    removing the commented card widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -47,45 +47,6 @@ class TransactionList extends StatelessWidget {
                         DateFormat.yMMMd().format(transactions[index].date)),
                   ),
                 );
-
-                //   Card(
-                //     child: Row(
-                //       children: <Widget>[
-                //         Container(
-                //           margin: EdgeInsets.symmetric(
-                //             vertical: 10,
-                //             horizontal: 15,
-                //           ),
-                //           decoration: BoxDecoration(
-                //               border: Border.all(
-                //             color: Theme.of(context).primaryColorDark,
-                //             width: 2,
-                //           )),
-                //           padding: EdgeInsets.all(10),
-                //           child: Text(
-                //               "\$${transactions[index].amount.toStringAsFixed(2)}",
-                //               style: TextStyle(
-                //                 fontWeight: FontWeight.bold,
-                //                 fontSize: 20,
-                //                 color: Theme.of(context).primaryColor,
-                //               )),
-                //         ),
-                //         Column(
-                //           crossAxisAlignment: CrossAxisAlignment.start,
-                //           children: <Widget>[
-                //             Text(
-                //               transactions[index].title,
-                //               style: Theme.of(context).textTheme.titleLarge,
-                //             ),
-                //             Text(
-                //               DateFormat.yMMMd().format(transactions[index].date),
-                //               style: TextStyle(fontSize: 12, color: Colors.grey),
-                //             )
-                //           ],
-                //         )
-                //       ],
-                //     ),
-                //   );
               },
               itemCount: transactions.length,
             ),
